8274d944166478628e288d6a979d01ccee7af879
GeorgeOndigo <georgeondigo8@gmail.com>
2025-03-26 17:09:13 +0300

    added salting to the hash for stronger security

--- include/user/auth.h
+++ include/user/auth.h
@@ -1,13 +1,18 @@
 #ifndef AUTH_H
 #define AUTH_H
 
-// Function to register a new user
+#define SALT_SIZE 16 // Lenght of a random salt
+
+//Function to generate a random salt
+void generate_salt(char *salt);
+
+// Function to register a new user ( store hashed password and salt)
 int register_user(const char *username, const char *password);
 
-// Function to authenticate a user (login)
+// Function to authenticate a user (login verify password with salt)
 int authenticate_user(const char *username, const char *password);
 
-// Function to hash a password using SHA-256
-void hash_password(const char *password, char *hashed_output);
+// Function to hash a password using SHA-256 and salt
+void hash_password(const char *password,  const char *salt, char *hashed_output);
 
 #endif // AUTH_H
--- src/user/auth.c
+++ src/user/auth.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <openssl/sha.h>
+#include <openssl/rand.h>
 #include "../../include/user/auth.h" 
 
 #define MAX_USERNAME_LEN 50
@@ -8,38 +9,50 @@
 #define HASH_SIZE 65 // 64 char for SHA-256 +1 for null terminator
 #define USER_DB "user.db"
 
+//Function to generate a random salt
+void generate_salt(char *salt){
+    RAND_bytes((unsigned char *)salt, SALT_SIZE);
+    salt[SALT_SIZE -1 ] = '\0'; // ensure null termination
+}
 
-// Function to hash a password using SHA-256
-void hash_password(const char *password, char *hashed_output){
+
+// Function to hash a password with salt using SHA-256
+void hash_password(const char *password,  const char *salt, char *hashed_output){
+    char combined[MAX_PASSWORD_LEN + SALT_SIZE];
+    snprintf(combined, sizeof(combined), "%s%s" , salt, password);
+
     unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256((unsigned char *)password, strlen(password), hash);
+    SHA256((unsigned char *)combined, strlen(combined), hash);
 
-    for ( int i = 0; i < SHA224_DIGEST_LENGTH; i++){
+    for ( int i = 0; i < SHA256_DIGEST_LENGTH; i++){
         sprintf(hashed_output + (i * 2), "%02x", hash[i]);
     }
     hashed_output[HASH_SIZE - 1] ='\0'; // Null-terminate the string
 }
 
-// Function to register user
+// Function to register user (store hashed password + salt)
 int register_user(const char *username, const char *password){
     FILE *file =fopen(USER_DB, "a"); // open user database in append mode
     if (!file){
         printf("Error opening user database.\n");
         return -1;
     }
-
+    
+    char salt[SALT_SIZE];
     char hashed_password[HASH_SIZE];
-    hash_password(password, hashed_password);
+
+    generate_salt(salt);
+    hash_password(password, salt, hashed_password);
 
     // Save user credentials to the file
-    fprintf(file, "%s %s\n", username, hashed_password);
+    fprintf(file, "%s %s %s\n", username, salt, hashed_password);
     fclose(file);
 
     printf("User %s registered sucessfully!\n", username);
     return 0;
 }
 
-//Function to authenticate user (login)
+//Function to authenticate user (login verify hashed password with salt)
 int authenticate_user(const char *username, const char *password){
     FILE *file =fopen(USER_DB, "r"); // open user database in read mode
     if(!file){
@@ -47,18 +60,21 @@ int authenticate_user(const char *username, const char *password){
         return -1;
     }
 
-    char stored_username[MAX_USERNAME_LEN], stored_password[HASH_SIZE];
+    char stored_username[MAX_USERNAME_LEN], stored_salt[SALT_SIZE], stored_password[HASH_SIZE];
     char hashed_input[HASH_SIZE];
 
-    hash_password(password, hashed_input); // Hash the input password
+    
 
     //check if the username and password matches a stored entry
-    while(fscanf(file, "%s %s", stored_username, stored_password) !=EOF){
-        if(strcmp(username, stored_username) == 0 && strcmp(hashed_input, stored_password) == 0){
-            fclose(file);
-            printf("Login sucessfull\n");
-            return 1;
-        }
+    while(fscanf(file, "%s %s %s", stored_salt, stored_password, stored_username) !=EOF){
+        if(strcmp(username, stored_username) == 0 ) {
+            hash_password(password, stored_salt, hashed_input); // Hash the input password plus salt
+            if (strcmp(hashed_input, stored_password) == 0){
+                fclose(file);
+                printf("Login sucessfull\n");
+                return 1;
+            }
+        } 
     }
 
     fclose(file);
