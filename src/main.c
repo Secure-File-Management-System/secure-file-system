b426a43a0ddff361980fde981ae67892d0c6774a
Dev-Awuor <Lloydawuor.dev@gmail.com>
2025-04-19 05:38:50 +0300

    Add option to view audit log in main menu

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "../include/user/auth.h"
+#include <string.h>
 #include "../include/security/logging.h"
 
 int main() {
     char username[50], password[50];
     int choice;
 
-    printf("1. Register \n2. Login\nChoose an option: ");
+    printf("1. Register \n2. Login\n3. View Audit Log\nChoose an option: ");
     scanf("%d", &choice);
 
     printf("Enter Username: ");
@@ -19,6 +20,8 @@ int main() {
         register_user(username, password);
     } else if (choice == 2) {
         authenticate_user(username, password);
+    }else if (choice == 3) {
+        view_log();       
     } else {
         printf("Invalid choice\n");
     }
